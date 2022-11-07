53d987d2aa0f519164c758c53f92c85998b4ad6b
sochacki111 <michal.sochacki111@gmail.com>
2022-11-07 13:40:17 +0100

    feat: graphql movies api

---
+++ src/movies/movies.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { AddMovieInput } from './inputs/add-movie.input';
+import { Movie } from './models/movie.model';
+
+@Injectable()
+export class MoviesService {
+  create(addMovieInput: AddMovieInput) {
+    return 'This action adds a new movie';
+  }
+
+  async findAll(): Promise<Movie[]> {
+    return [
+      {
+        id: 1,
+        title: 'test',
+        directorId: 1,
+      },
+      {
+        id: 2,
+        title: 'test2',
+        directorId: 2,
+      },
+    ];
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} movie`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} movie`;
+  }
+}
